fb827c6b523c53d397ff2b707252cf3f67b6b94a
cducrest <come.ducrest@gmail.com>
2019-06-04 11:41:32 +0200

    Add test for deploying with evm target st petersburg

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -3,11 +3,22 @@ import eth_utils
 from eth_utils import is_address
 
 from deploy_tools.deploy import deploy_compiled_contract, send_function_call_transaction
+from deploy_tools.plugin import get_contracts_folder
+from deploy_tools.compile import compile_project
 
 
 @pytest.fixture()
 def test_contract(deploy_contract):
     return deploy_contract("TestContract", constructor_args=(4,))
+
+
+@pytest.fixture()
+def contract_assets_st_petersburg(pytestconfig):
+    contracts_path = get_contracts_folder(pytestconfig)
+    evm_version = "petersburg"
+    return compile_project(
+        contracts_path=contracts_path, optimize=True, evm_version=evm_version
+    )
 
 
 def test_deploy_from_private_key(web3, contract_assets, account_keys):
@@ -25,6 +36,18 @@ def test_deploy_from_private_key(web3, contract_assets, account_keys):
 
 def test_deploy(web3, contract_assets):
     contract_interface = contract_assets["TestContract"]
+    contract = deploy_compiled_contract(
+        abi=contract_interface["abi"],
+        bytecode=contract_interface["bytecode"],
+        web3=web3,
+        constructor_args=(1,),
+    )
+
+    assert is_address(contract.address)
+
+
+def test_deploy_st_petersburg(web3, contract_assets_st_petersburg):
+    contract_interface = contract_assets_st_petersburg["TestContract"]
     contract = deploy_compiled_contract(
         abi=contract_interface["abi"],
         bytecode=contract_interface["bytecode"],
